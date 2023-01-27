c9aad2b0ace1a2969bfdfffe2b136276b3db9dae
SM Harwood <stuart.m.harwood@exxonmobil.com>
2023-01-27 14:27:48 -0500

    Central unittest runner

--- src/run_unittest.py
+++ src/run_unittest.py
@@ -4,26 +4,39 @@ SM Harwood
 
 Run unit-ish tests for vrpqubo package
 """
+import os
+import argparse
+import importlib
 import logging
-from vrpqubo.tests import (
-    test_arc_based,
-    test_path_based,
-    test_sequence_based,
-    test_mirp_g1,
-    test_mirp_g1_feas,
-    test_sampler
-)
+import re
 
 logging.basicConfig(level=logging.DEBUG)
 
 def main():
-    """ Run unit test """
-    # test_arc_based.test()
-    test_path_based.test()
-    # test_sequence_based.test()
-    # test_mirp_g1.test()
-    # test_mirp_g1_feas.test()
-    # test_sampler.test()
+    """ Run unit tests """
+    parser = argparse.ArgumentParser(description=
+        "Run unittests in vrpqubo.tests",
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument("tests", nargs='+',
+                        help="Name of test(s) to run, or 'all'")
+    args = parser.parse_args()
+
+    reg = re.compile(r"test_.*py")
+    if args.tests[0] == "all":
+        tests = os.listdir(os.path.join("vrpqubo","tests"))
+        tests = filter(lambda fn: (reg.match(fn) is not None), tests)
+    else:
+        tests = args.tests
+
+    # go through the given tests, and try to run test() method
+    for test in tests:
+        test = test.split('.')[0]
+        print("\n")
+        try:
+            test_mod = importlib.import_module(f"vrpqubo.tests.{test}")
+            test_mod.test()
+        except (ModuleNotFoundError, AttributeError) as error:
+            print(error)
 
 if __name__ == "__main__":
     main()
