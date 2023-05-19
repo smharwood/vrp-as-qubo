aa9192fe26a997a87ec49c9d2ab51c7a960da540
SM Harwood <stuart.m.harwood@exxonmobil.com>
2023-05-19 14:01:52 -0400

    Better random MIRP

--- src/vrpqubo/examples/mirp_random.py
+++ src/vrpqubo/examples/mirp_random.py
@@ -20,10 +20,11 @@ def sample(vari, size: int=1):
     """
     if isinstance(vari, Sampleable_Type):
         return vari.rvs(size)
+    # else, if the right size, return the object itself
     if np.isscalar(vari) and size == 1:
-        # else, if the right size, return the object itself
         return vari
-    if len(vari) == size:
+    # This first clause is necessary to short-circuit when vari has no len
+    if not np.isscalar(vari) and len(vari) == size:
         return vari
     raise ValueError("Variable to sample is not the right length")
 
@@ -48,141 +49,143 @@ class RandomMIRP:
     demand_port_fees: Optional[Union[ArrayLike,Sampleable]]=None
     seed: Optional[int]=None
 
-    def get_random_mirp(self, size: int=1):
-        """
-        Get random instance(s) of the MIRP
-        """
-        # set random seed
+    def __post_init__(self):
         np.random.seed(self.seed)
 
-        mirps = []
+    def get_random_mirp(self, reset_seed: bool=False):
+        """
+        Get random instance of the MIRP
+        """
+        if reset_seed:
+            # set random seed
+            np.random.seed(self.seed)
+
+        # For any input that is a distribution/random variable, sample it
+        time_horizon = sample(self.time_horizon)
+        cargo_size = sample(self.cargo_size)
+        num_supply_ports = sample(self.num_supply_ports)
+        num_demand_ports = sample(self.num_demand_ports)
+
+        # Supply
+        inventory_init_supply = sample(self.inventory_init_supply, size=num_supply_ports)
+        assert (inventory_init_supply > 0).all(), "Supply initial inventory should be positive"
+        assert len(inventory_init_supply) == num_supply_ports, \
+            "Length of inventory_init_supply is incorrect"
+
+        inventory_rate_supply = sample(self.inventory_rate_supply, size=num_supply_ports)
+        assert (inventory_rate_supply > 0).all(), "Supply inventory rate should be positive"
+        assert len(inventory_rate_supply) == num_supply_ports, \
+            "Length of inventory_rate_supply is incorrect"
+
+        inventory_cap_supply = sample(self.inventory_cap_supply, size=num_supply_ports)
+        assert (inventory_cap_supply > 0).all(), "Supply inventory capacity should be positive"
+        assert len(inventory_cap_supply) == num_supply_ports, \
+            "Length of inventory_cap_supply is incorrect"
+
+        # Demand
+        inventory_init_demand = sample(self.inventory_init_demand, size=num_demand_ports)
+        assert (inventory_init_demand > 0).all(), "demand initial inventory should be positive"
+        assert len(inventory_init_demand) == num_demand_ports, \
+            "Length of inventory_init_demand is incorrect"
+
+        inventory_rate_demand = sample(self.inventory_rate_demand, size=num_demand_ports)
+        assert (inventory_rate_demand < 0).all(), "demand inventory rate should be negative"
+        assert len(inventory_rate_demand) == num_demand_ports, \
+            "Length of inventory_rate_demand is incorrect"
+
+        inventory_cap_demand = sample(self.inventory_cap_demand, size=num_demand_ports)
+        assert (inventory_cap_demand > 0).all(), "demand inventory capacity should be positive"
+        assert len(inventory_cap_demand) == num_demand_ports, \
+            "Length of inventory_cap_demand is incorrect"
+
+        # Travel arcs
+        n_total = num_supply_ports + num_demand_ports
+        if isinstance(self.travel_times, Sampleable_Type):
+            travel_times = self.travel_times.rvs(size=(n_total,n_total))
+            # zero out diagonal and make symmetric
+            # set lower triangle equal to upper triangle
+            for i in range(n_total):
+                travel_times[i,i] = 0
+                for j in range(i+1,n_total):
+                    travel_times[j,i] = travel_times[i,j]
+        else:
+            travel_times = np.asarray(self.travel_times)
+        assert (travel_times >= 0).all(), "Travel times should be non-negative"
+        assert travel_times.shape == (n_total,n_total),\
+            "Shape of travel_times is incorrect"
+
+        if self.travel_cost_per_unit_time is None:
+            # travel time will be proxy for cost
+            travel_cost_per_unit_time = 1.0
+        else:
+            travel_cost_per_unit_time = sample(self.travel_cost_per_unit_time)
+        assert travel_cost_per_unit_time >= 0,\
+            "Travel cost per unit time should be non-negative"
+
+        if self.supply_port_fees is None:
+            supply_port_fees = np.zeros(num_supply_ports)
+        else:
+            supply_port_fees = sample(self.supply_port_fees, size=num_supply_ports)
+        assert len(supply_port_fees) == num_supply_ports,\
+            "Length of supply_port_fees is incorrect"
+
+        if self.demand_port_fees is None:
+            demand_port_fees = np.zeros(num_demand_ports)
+        else:
+            demand_port_fees = sample(self.demand_port_fees, size=num_demand_ports)
+        assert len(demand_port_fees) == num_demand_ports,\
+            "Length of demand_port_fees is incorrect"
+
+        # Construct MIRP object
+        mirp = MIRP(cargo_size, time_horizon)
+
+        # Add nodes
+        # Supply
+        sp_fees = {}
+        for i, (init, rate, cap) in enumerate(
+            zip(inventory_init_supply, inventory_rate_supply, inventory_cap_supply)
+        ):
+            name = f"S{i+1}"
+            mirp.add_nodes(name, init, rate, cap)
+            sp_fees[name] = supply_port_fees[i]
+        # Demand
+        dp_fees = {}
+        for i, (init, rate, cap) in enumerate(
+            zip(inventory_init_demand, inventory_rate_demand, inventory_cap_demand)
+        ):
+            name = f"D{i+1}"
+            mirp.add_nodes(name, init, rate, cap)
+            dp_fees[name] = demand_port_fees[i]
+
+        # Add arcs
+        # "Distance function" will just give travel times, and we will use
+        # a vessel speed of 1
+        # This function gets used immediately then we don't care anymore
+        # so theres no late binding issue
+        combined_ports = mirp.supply_ports + mirp.demand_ports
+        def distance_function(port1, port2):
+            i = combined_ports.index(port1)
+            j = combined_ports.index(port2)
+            return travel_times[i][j]
+        mirp.add_travel_arcs(
+            distance_function,
+            vessel_speed=1,
+            cost_per_unit_distance=travel_cost_per_unit_time,
+            supply_port_fees=sp_fees,
+            demand_port_fees=dp_fees
+        )
+        mirp.add_exit_arcs()
+        # How many vessels should we have?
+        # ultimately, the problem will get modified when the specific
+        # formulations are constructed.
+        # But, some initial entry arcs are probably a good idea...
+        mirp.add_entry_arcs(time_limit=time_horizon/10)
+        return mirp
+
+    def random_mirp_gen(self, size: int=1):
+        """Get generator for instances of random mirps"""
         for _ in range(size):
-            # For any input that is a distribution/random variable, sample it
-            time_horizon = sample(self.time_horizon)
-            cargo_size = sample(self.cargo_size)
-            num_supply_ports = sample(self.num_supply_ports)
-            num_demand_ports = sample(self.num_demand_ports)
-
-            # Supply
-            inventory_init_supply = sample(self.inventory_init_supply, size=num_supply_ports)
-            assert (inventory_init_supply > 0).all(), "Supply initial inventory should be positive"
-            assert len(inventory_init_supply) == num_supply_ports, \
-                "Length of inventory_init_supply is incorrect"
-
-            inventory_rate_supply = sample(self.inventory_rate_supply, size=num_supply_ports)
-            assert (inventory_rate_supply > 0).all(), "Supply inventory rate should be positive"
-            assert len(inventory_rate_supply) == num_supply_ports, \
-                "Length of inventory_rate_supply is incorrect"
-
-            inventory_cap_supply = sample(self.inventory_cap_supply, size=num_supply_ports)
-            assert (inventory_cap_supply > 0).all(), "Supply inventory capacity should be positive"
-            assert len(inventory_cap_supply) == num_supply_ports, \
-                "Length of inventory_cap_supply is incorrect"
-
-            # Demand
-            inventory_init_demand = sample(self.inventory_init_demand, size=num_demand_ports)
-            assert (inventory_init_demand > 0).all(), "demand initial inventory should be positive"
-            assert len(inventory_init_demand) == num_demand_ports, \
-                "Length of inventory_init_demand is incorrect"
-
-            inventory_rate_demand = sample(self.inventory_rate_demand, size=num_demand_ports)
-            assert (inventory_rate_demand < 0).all(), "demand inventory rate should be negative"
-            assert len(inventory_rate_demand) == num_demand_ports, \
-                "Length of inventory_rate_demand is incorrect"
-
-            inventory_cap_demand = sample(self.inventory_cap_demand, size=num_demand_ports)
-            assert (inventory_cap_demand > 0).all(), "demand inventory capacity should be positive"
-            assert len(inventory_cap_demand) == num_demand_ports, \
-                "Length of inventory_cap_demand is incorrect"
-
-            # Travel arcs
-            n_total = num_supply_ports + num_demand_ports
-            if isinstance(self.travel_times, Sampleable_Type):
-                travel_times = self.travel_times.rvs(size=(n_total,n_total))
-                # zero out diagonal and make symmetric
-                # set lower triangle equal to upper triangle
-                for i in range(n_total):
-                    travel_times[i,i] = 0
-                    for j in range(i+1,n_total):
-                        travel_times[j,i] = travel_times[i,j]
-            else:
-                travel_times = np.asarray(self.travel_times)
-            assert (travel_times >= 0).all(), "Travel times should be non-negative"
-            assert travel_times.shape == (n_total,n_total),\
-                "Shape of travel_times is incorrect"
-
-            if self.travel_cost_per_unit_time is None:
-                # travel time will be proxy for cost
-                travel_cost_per_unit_time = 1.0
-            else:
-                travel_cost_per_unit_time = sample(self.travel_cost_per_unit_time)
-            assert travel_cost_per_unit_time >= 0,\
-                "Travel cost per unit time should be non-negative"
-
-            if self.supply_port_fees is None:
-                supply_port_fees = np.zeros(num_supply_ports)
-            else:
-                supply_port_fees = sample(self.supply_port_fees, size=num_supply_ports)
-            assert len(supply_port_fees) == num_supply_ports,\
-                "Length of supply_port_fees is incorrect"
-
-            if self.demand_port_fees is None:
-                demand_port_fees = np.zeros(num_demand_ports)
-            else:
-                demand_port_fees = sample(self.demand_port_fees, size=num_demand_ports)
-            assert len(demand_port_fees) == num_demand_ports,\
-                "Length of demand_port_fees is incorrect"
-
-            # Construct MIRP object
-            mirp = MIRP(cargo_size, time_horizon)
-
-            # Add nodes
-            # Supply
-            sp_fees = {}
-            for i, (init, rate, cap) in enumerate(
-                zip(inventory_init_supply, inventory_rate_supply, inventory_cap_supply)
-            ):
-                name = f"S{i+1}"
-                mirp.add_nodes(name, init, rate, cap)
-                sp_fees[name] = supply_port_fees[i]
-            # Demand
-            dp_fees = {}
-            for i, (init, rate, cap) in enumerate(
-                zip(inventory_init_demand, inventory_rate_demand, inventory_cap_demand)
-            ):
-                name = f"D{i+1}"
-                mirp.add_nodes(name, init, rate, cap)
-                dp_fees[name] = demand_port_fees[i]
-
-            # Add arcs
-            # "Distance function" will just give travel times, and we will use
-            # a vessel speed of 1
-            # This function gets used immediately then we don't care anymore
-            # so theres no late binding issue
-            combined_ports = mirp.supply_ports + mirp.demand_ports
-            def distance_function(port1, port2):
-                i = combined_ports.index(port1)
-                j = combined_ports.index(port2)
-                return travel_times[i][j]
-            mirp.add_travel_arcs(
-                distance_function,
-                vessel_speed=1,
-                cost_per_unit_distance=travel_cost_per_unit_time,
-                supply_port_fees=sp_fees,
-                demand_port_fees=dp_fees
-            )
-            mirp.add_exit_arcs()
-            # How many vessels should we have?
-            # ultimately, the problem will get modified when the specific
-            # formulations are constructed.
-            # But, some initial entry arcs are probably a good idea...
-            mirp.add_entry_arcs(time_limit=time_horizon/10)
-            mirps.append(mirp)
-        # end for loop
-
-        if size == 1:
-            return mirps[0]
-        return mirps
+            yield self.get_random_mirp()
 
 
 def get_generator(
@@ -194,7 +197,7 @@ def get_generator(
         time_between_windows: Optional[Sampleable]=None
     ) -> RandomMIRP:
     """
-    RandomMIRP is maybe TOO random to be useful
+    RandomMIRP is maybe TOO random to be useful.
     This method will try to give some flexibility while setting some other
     values to sensible defaults.
     """
--- src/vrpqubo/tests/test_mirp_random.py
+++ src/vrpqubo/tests/test_mirp_random.py
@@ -4,23 +4,43 @@ SM Harwood
 """
 import logging
 import unittest
-from ..examples.mirp_random import get_generator
-# from matplotlib import pyplot as plt
+from ..examples.mirp_random import get_generator, sample
 
 class TestMirpRandom(unittest.TestCase):
-    """Subclass unittest.TestCase to test random MIRP generator"""
+    """ Test random MIRP generator"""
     logger = logging.getLogger(__name__)
 
     def test(self):
-        """ Basic test """
+        """ Test sizing of Random MIRP """
+        mirp_gen = get_generator(
+            num_supply_ports=2,
+            num_demand_ports=2,
+            time_horizon=100
+        )
+        mirp = mirp_gen.get_random_mirp()
+        self.logger.debug(mirp)
+        self.assertEqual(len(mirp.supply_ports), 2, "Wrong number of supply ports")
+        self.assertEqual(len(mirp.demand_ports), 2, "Wrong number of demand ports")
+        self.assertEqual(mirp.time_horizon, 100, "Time horizon is incorrect")
+        return
+
+    def test_generator(self):
+        """ Test generator of random MIRPs """
         mirp_gen = get_generator(
             num_supply_ports=1,
             num_demand_ports=1,
             time_horizon=100
         )
-        mirp = mirp_gen.get_random_mirp(1)
-        self.logger.debug(mirp)
-        self.assertEqual(len(mirp.supply_ports), 1, "Wrong number of supply ports")
-        self.assertEqual(len(mirp.demand_ports), 1, "Wrong number of demand ports")
-        self.assertEqual(mirp.time_horizon, 100, "Time horizon is incorrect")
-        return
+        mirps = mirp_gen.random_mirp_gen(2)
+        for mirp in mirps:
+            self.assertEqual(len(mirp.supply_ports), 1, "Wrong number of supply ports")
+            self.assertEqual(len(mirp.demand_ports), 1, "Wrong number of demand ports")
+            self.assertEqual(mirp.time_horizon, 100, "Time horizon is incorrect")
+
+    def test_sample(self):
+        """ Test `sample` function, and how it handles non-sampleables """
+        s = sample([1,2,3], size=3)
+        self.assertEqual(s, [1,2,3], "Sample is not correct")
+        s = sample(1.0, size=1)
+        self.assertEqual(s, 1.0, "Sample is not correct")
+        self.assertRaises(ValueError, sample, 1.0, size=3)
